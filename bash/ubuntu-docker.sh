409da3df22dbe76c92d32673fefcddc0324d649e
vinic <vinicius.cassemira@fatec.sp.gov.br>
2025-07-09 10:55:48 -0300

    feat: docker installation

---
+++ bash/ubuntu-docker.sh
@@ -0,0 +1,28 @@
+# Download docker script install to use
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+# Testing installation
+if docker compose -v &> /dev/null; then
+  echo "docker installed successfully"
+else
+  echo "docker not installed"
+  exit 1
+fi
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+sudo chown $USER:$USER /home/$USER/.docker -R
+sudo chmod g+rwx $HOME/.docker -R
+
+sudo apt update
+sudo apt install -y docker-compose-plugin
+
+# Testing installation
+if docker compose -v &> /dev/null; then
+  echo "docker compose installed successfully"
+else
+  echo "docker compose not installed"
+  exit 1
+fi
