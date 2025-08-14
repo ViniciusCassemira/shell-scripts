880dadda637a48b7a5b98c6e721fda3ce6b06030
ViniciusCassemira <vinicius.cassemira@fatec.sp.gov.br>
2025-08-14 15:45:49 -0300

    💚 ci/cd: update ssh keys config github/gitlab

--- bash/ssh_host_key_config.sh
+++ bash/ssh_host_key_config.sh
@@ -1,27 +1,66 @@
-# Generate keys
-ssh-keygen -t ed25519 -f "~/.ssh/gitlab-proj1" -N ""
-ssh-keygen -t ed25519 -f "~/.ssh/gitlab-proj2" -N ""
-echo "Keys generated successfully"
+max_key=4
 
-# Config ~/.ssh/config file
-echo "Host gitlab-proj1
- HostName gitlab.com
- User git
- IdentityFile ~/.ssh/gitlab-proj1
-Host gitlab-proj2
- HostName gitlab.com
- User git
- IdentityFile ~/.ssh/gitlab-proj2" > ~/.ssh/config
-echo "~/.ssh/config configured successfully:"
-cat ~/.ssh/config
+while true; do
+    read -p "How many projects do you want to configure? " numberProjects
+    
+    if [[ "$numberProjects" -lt $((max_key + 1)) ]] && [[ "$numberProjects" -gt 0 ]]; then
+        echo "Configuring $numberProjects project(s)..."
+        break
+    else
+        echo "Please choose a number between 1 and $max_key"
+    fi
+done
 
-# Displaying public keys that were generated
-echo "Use these keys to configure your Deploy keys in GitLab project"
-echo "Proj1 .pub key"
-cat ~/.ssh/gitlab-proj1.pub
-echo "Proj2 .pub key"
-cat ~/.ssh/gitlab-proj2.pub
+while true; do
+    read -p "Out of $numberProjects projects, how many are on GitHub? " githubProjects
+    if [[ "$githubProjects" -gt "$numberProjects" ]] || [[ "$githubProjects" -lt 0 ]]; then
+        echo "Please enter a valid value."
+    else
+        gitlabProjects=$((numberProjects - githubProjects))
+        echo "Summary:"
+        echo "GitHub projects: $githubProjects"
+        echo "GitLab projects: $gitlabProjects"
+        
+        read -p "Confirm configurations before starting? (y/N): " confirmProjects
+        if [[ "${confirmProjects,,}" == "y" ]]; then
+            clear
+            echo "Configuring $numberProjects project(s)..."
+            break
+        else
+            echo "Process canceled"
+            exit 1
+        fi
+    fi
+done
 
-echo "To git yout project, use this syntax:"
-echo "git clone git@gitlab-proj1:username/project1.git"
+for ((i=1; i<=$githubProjects; i++)); do
+    echo "GitHub project: $i"
+    ssh-keygen -t ed25519 -f "$HOME/.ssh/github-proj$i" -N "" -q
+
+    echo "Host github-proj$i
+        HostName github.com
+        User git
+        IdentityFile ~/.ssh/github-proj$i" >> ~/.ssh/config
+
+    echo "GitHub key #$i generated. Use the public key in your project's Deploy Keys:"
+    cat ~/.ssh/github-proj$i.pub
+done
+
+for ((i=1; i<=$gitlabProjects; i++)); do
+    echo "GitLab project: $i"
+    ssh-keygen -t ed25519 -f "$HOME/.ssh/gitlab-proj$i" -N "" -q
+
+    echo "Host gitlab-proj$i
+        HostName gitlab.com
+        User git
+        IdentityFile ~/.ssh/gitlab-proj$i" >> ~/.ssh/config
+    
+    echo "GitLab key #$i generated. Use the public key in your project's Deploy Keys:"
+    cat ~/.ssh/gitlab-proj$i.pub
+done
+
+echo "To git your project, use this syntax:"
+echo "git clone git@gitlab-proj1:username/project1.git (or github)"
 echo "[Example] - git clone git@gitlab-proj1:ViniciusCassemira/gitlab-ci-connect-server.git"
+echo "If your project is already on the server, access it and run:"
+echo "git remote set-url origin git@gitlab-proj1:username/project1.git"
